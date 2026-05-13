475251dab530b5809ff8d9c8da93f5abb7832733
Brian Martin <brayniac@gmail.com>
2026-05-13 08:48:56 -0700

    feat(telemetry): tracing init + Prometheus recorder

--- src/telemetry/mod.rs
+++ src/telemetry/mod.rs
@@ -1,1 +1,25 @@
-//! TODO: module documentation will be added as functionality lands.
+use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
+use tracing_subscriber::{prelude::*, EnvFilter};
+
+pub fn init_tracing() {
+    let filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info,barry_bot=debug"));
+    let fmt = tracing_subscriber::fmt::layer().json();
+    let _ = tracing_subscriber::registry().with(filter).with(fmt).try_init();
+}
+
+pub fn install_metrics() -> PrometheusHandle {
+    PrometheusBuilder::new()
+        .install_recorder()
+        .expect("install Prometheus recorder")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn install_metrics_returns_handle() {
+        let h = install_metrics();
+        let _ = h.render();
+    }
+}
